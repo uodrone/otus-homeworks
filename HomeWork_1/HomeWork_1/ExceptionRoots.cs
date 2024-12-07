119814e2c37b9e9c1508a449825c45afc995e8b3
uodrone <drone_86@mail.ru>
2024-12-07 14:22:06 +0400

    Домашнее задание 3, практика исключений

---
+++ HomeWork_1/HomeWork_1/ExceptionRoots.cs
@@ -0,0 +1,10 @@
+namespace OTUS_Homework_3
+{
+    internal class ExeptionRoots
+    {
+        public void SendExeptionRoots ()
+        {
+            throw new Exception("Вещественных значений не найдено");
+        }
+    }
+}
--- HomeWork_1/HomeWork_1/Program.cs
+++ HomeWork_1/HomeWork_1/Program.cs
@@ -1,111 +1,157 @@
-﻿using System.Data.SqlTypes;
+﻿using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace HomeWork_1
+namespace OTUS_Homework_3
 {
-    internal class Program
+    internal class QuadraticEquation
     {
+        enum Severity
+        {
+            Warning,
+            Error
+        }
+
         static void Main(string[] args)
         {
-            StartMessage(null);
-        }
-
-        static void StartMessage (string? UserName)
-        {
-            Console.WriteLine(); //создаем отступ для удобочитаемости
-            string AdditionText = "дай";
-            string UserDisplayName = UserName;
-            string? Command;
-            if (string.IsNullOrEmpty(UserName))
-            {
-                AdditionText = "/start набери";
-                UserDisplayName = "гуманоид";
-            }
-
-            Console.WriteLine("Приветствую тебя, о " + UserDisplayName + "!\r\nЯ — Вояджер-17, твой чат-бот.");
-            Console.WriteLine("Хочешь начать общение со мной?\r\nТогда команду " + AdditionText + " и вперёд!");
-            Console.WriteLine("Если не знаешь, как пользоваться мной,\r\nНабери /help, я тебе помогу.");
-            Console.WriteLine("А если хочешь узнать, кто я такой,\r\nТо спроси меня /info, всё тут я расскажу.");
-            if (!string.IsNullOrEmpty(UserName))
-            {
-                Console.WriteLine($"{UserName}!\r\nХочешь эхо услыхать?\r\nНабери команду /echo с аргументом впереди.\r\nЯ готов тебе внимать!");
-            }
-             Console.WriteLine("Надоело со мной говорить?\r\nПросто скажи мне /exit, и можешь уходить!");
-
-            do
-            {
-                Command = Console.ReadLine();
-                UserName = CommandHandler(Command, UserName);
-            } while (Command != "/exit");
+            Console.WriteLine("Попробуем решить квадратное уравнение a*x^2 + b*x + c = 0");
+            HandlerExeption();
         }
 
         /// <summary>
-        /// Обработчик команд пользователя
+        /// Запрашиваем данные и обрабатываем исключения
         /// </summary>
-        /// <param name="Command"></param>
-        /// <param name="UserName"></param>
-        /// <returns></returns>
-        static string CommandHandler(string Command, string UserName)
-        {            
-            Console.WriteLine(); //создаем отступ для удобочитаемости
-            string UserDisplayName = string.IsNullOrEmpty(UserName) ? "Гуманоид" : UserName;
+        static void HandlerExeption()
+        {
+            var Arguments = new Dictionary<string, int>();
+            var ArgsCheckFormat = new Dictionary<string, string>();
+            var ListErrorArgs = new List<string>();
 
-            switch (Command)
+            GetArgument("a", ref Arguments, ref ArgsCheckFormat, ref ListErrorArgs);
+            GetArgument("b", ref Arguments, ref ArgsCheckFormat, ref ListErrorArgs);
+            GetArgument("c", ref Arguments, ref ArgsCheckFormat, ref ListErrorArgs);
+
+            try
             {
-                case "/start":
-                    if (string.IsNullOrEmpty(UserName))
-                    {
-                        UserName = GetName();
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Я уже знаю что тебя зовут {UserName}, и готов ответить твои вопросы");
-                    }                    
-                    break;
-                case "/help":
-                    Console.WriteLine($"{UserDisplayName}, чтоб ботом пользоваться умело, выбирай команды смело!");
-                    break;
-                case "/info":
-                    DateTime DateCreate = new DateTime(DateTime.Now.Year, 11, 16, 16, 0, 0);
-                    Console.WriteLine($"{UserDisplayName}, меня зовут Вояджер-17, версия 17.01.28.1. Дата создания: {DateCreate}");
-                    break;
-                default:
-                    if (string.IsNullOrEmpty(Command))
-                    {
-                        Console.WriteLine("Ничего ты не ответил, гуманоид дорогой,\r\nЧто тревожит твою душу, беспокоит твой покой.");
-                        Console.WriteLine("Поразмысли о желаньях и теперь мне сообщи,\r\nЧто за выберешь команду, путь ты свой мне укажи.\r\n");                        
-                    }
-                    else if (Command.StartsWith("/echo ") && !string.IsNullOrEmpty(UserName))
-                    {
-                        Console.WriteLine(Command.Replace("/echo ", ""));
-                    }
-                    else
-                    {
-                        Console.WriteLine("Команды я такой не знаю,\r\nТы расскажи мне всё равно.");
-                        Console.WriteLine("Что за команду ожидаешь\r\nЗдесь в выполнении моём?\r\n");                        
-                    }                    
-                    break;
+                if (ListErrorArgs.Count > 0)
+                {
+                    string ErrorArgs = string.Join(", ", ListErrorArgs);
+                    string EndWord = ListErrorArgs.Count > 1 ? "ов" : "а";
+
+                    throw new FormatException($"Неверный формат параметр{EndWord}: {ErrorArgs}");
+                }
+                else
+                {
+                    GetRoots(Arguments);
+                }
             }
-            Console.WriteLine("А пока попробуй другие команды");
-            Console.WriteLine(); //создаем отступ для удобочитаемости
-            return UserName;
+            catch (FormatException fex)
+            {
+                FormatData(fex.Message, Severity.Error, ArgsCheckFormat);
+                Console.WriteLine();
+                Console.WriteLine("Начинаем все сначала:");
+                HandlerExeption();
+            }
+            catch (Exception ex) {
+                FormatData(ex.Message, Severity.Warning, ArgsCheckFormat);
+            }
         }
 
         /// <summary>
-        /// Получение имени пользователя
+        /// Метод получения аргумента уравнения
         /// </summary>
-        /// <returns></returns>
-        static string GetName()
+        /// <param name="ArgName"></param>
+        /// <param name="Arguments"></param>
+        /// <param name="ArgsCheckFormat"></param>
+        static void GetArgument (string ArgName, ref Dictionary<string, int> Arguments, ref Dictionary<string, string> ArgsCheckFormat, ref List<string> ListErrorArgs)
         {
-            Console.WriteLine("Поведай имя мне своё");
-            string UserName = Console.ReadLine();
+            Console.WriteLine($"Введите значение {ArgName}:");
+            string ArgValString = Console.ReadLine();
+            ArgsCheckFormat.Add(ArgName, ArgValString);
+            bool result = int.TryParse(ArgValString, out int ArgVal);
+            Arguments.Add(ArgName, ArgVal);
+            if (!result)
+            {
+                ListErrorArgs.Add(ArgName);
+            }
+        }
 
-            while (string.IsNullOrEmpty(UserName)) {
-                Console.WriteLine("Смотрите какой таинственный! Попробуй еще раз.");
-                UserName = Console.ReadLine();
+        /// <summary>
+        /// Получаем дискриминант
+        /// </summary>
+        /// <param name="Arguments"></param>
+        /// <returns></returns>
+        static int GetDiscriminant (Dictionary<string, int> Arguments)
+        {
+            int D = Arguments["b"]*Arguments["b"] - 4*Arguments["a"]*Arguments["c"];
+            return D;
+        }
+
+        /// <summary>
+        /// Ищем корни, а если их нет - пробрасываем исключение
+        /// </summary>
+        /// <param name="Arguments"></param>
+        /// <exception cref="Exception"></exception>
+        static void GetRoots (Dictionary<string, int> Arguments)
+        {
+            int D = GetDiscriminant(Arguments);
+            int a = Arguments["a"];
+            int b = Arguments["b"];            
+
+            if (D < 0)
+            {
+                var ExceptionRoots = new ExeptionRoots();
+                ExceptionRoots.SendExeptionRoots();
+            }
+            else if (D == 0) {
+                double x = (Math.Sqrt(D) - b) / 2 * a;
+                Console.WriteLine("x = " + x);
+            }
+            else
+            {
+                double x1 = (Math.Sqrt(D) - b) / 2 * a;
+                double x2 = (-Math.Sqrt(D) - b) / 2 * a;
+
+                Console.WriteLine("x1 = " + x1);
+                Console.WriteLine("x2 = " + x2);
             }
 
-            Console.WriteLine("Приятно познакомиться!");
-            return UserName;
+            Console.WriteLine("Поздравляю, Вы гениальны!");
+        }
+
+        /// <summary>
+        /// Определяем формат вывода исключения
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="severity"></param>
+        /// <param name="data"></param>
+        static void FormatData(string message, Severity severity, IDictionary<string, string> data)
+        {
+            //устанавливаем цвета консоли в зависимости от типа ошибки
+            if (severity == Severity.Error) {
+                Console.BackgroundColor = ConsoleColor.Red;
+            } else
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.Yellow;
+            }
+
+            for (int i = 0; i < 50; i++) {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+            Console.WriteLine(message);
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < data.Count; i++) {
+                Console.WriteLine($"{data.Keys.ElementAt(i)} = {data[data.Keys.ElementAt(i)]}");
+            }
+
+            //Сбрасываем цвета консоли, чтоб не дай бох дальше таким писать
+            Console.ResetColor();
         }
     }
 }
